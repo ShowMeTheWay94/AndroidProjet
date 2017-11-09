5092c70b91866716a47e62053e9e3b2b582f14c4
bastien <bastiendewez@hotmail.com>
2017-11-09 21:30:09 +0100

    Projet 6

---
+++ Projet6/app/src/main/java/com/example/basti/projet6/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.basti.projet6;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMenu = (Button)findViewById(R.id.btnMenu);
+        btnMenu.setOnClickListener(listener);
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v)
+        {
+            Intent intent = new Intent(MainActivity.this, ChoixPersonnage.class);
+            startActivity(intent);
+        }
+    };
+}
